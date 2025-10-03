9cb79ee24963947000f4b4f1df2c7ab90b4fc8f3
H-bond826 <zhuqingjian826@gmail.com>
2025-10-03 07:43:07 +0200

    script

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# TP-SVM
+# TP-HAX907X-Apprentissage-statistique
+
+Ce dépôt contient le code source et le rapport pour le TP sur les Machines à Vecteurs de Support (SVM).
+
+## Informations sur le groupe
+
+- **Auteur :** Qingjian ZHU
--- src/TP SVM.py
+++ src/TP SVM.py
@@ -20,9 +20,9 @@ warnings.filterwarnings("ignore")
 
 plt.style.use('ggplot')
 # %%
-############
-# Iris Dataset
-############
+###################################################
+#               Iris Dataset
+###################################################
 iris = datasets.load_iris()
 X = iris.data
 X = scaler.fit_transform(X)
@@ -100,4 +100,210 @@ plt.title("polynomial kernel")
 plt.tight_layout()
 plt.draw()
 
+
+
+
+#%%
+###################################################
+#               Face Recognition Task
+###################################################
+"""
+The dataset used in this example is a preprocessed excerpt
+of the "Labeled Faces in the Wild", aka LFW_:
+
+  http://vis-www.cs.umass.edu/lfw/lfw-funneled.tgz (233MB)
+
+  _LFW: http://vis-www.cs.umass.edu/lfw/
+"""
+
+####################################################################
+# Download the data and unzip; then load it as numpy arrays
+lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4,
+                              color=True, funneled=False, slice_=None,
+                              download_if_missing=True)
+# data_home='.'
+
+# introspect the images arrays to find the shapes (for plotting)
+images = lfw_people.images
+n_samples, h, w, n_colors = images.shape
+
+# the label to predict is the id of the person
+target_names = lfw_people.target_names.tolist()
+
+####################################################################
+# Pick a pair to classify such as
+names = ['Tony Blair', 'Colin Powell']
+# names = ['Donald Rumsfeld', 'Colin Powell']
+
+idx0 = (lfw_people.target == target_names.index(names[0]))
+idx1 = (lfw_people.target == target_names.index(names[1]))
+images = np.r_[images[idx0], images[idx1]]
+n_samples = images.shape[0]
+y = np.r_[np.zeros(np.sum(idx0)), np.ones(np.sum(idx1))].astype(int)
+
+# plot a sample set of the data
+plot_gallery(images, np.arange(12))
+plt.show()
+
+#%%
+####################################################################
+# Extract features
+
+# features using only illuminations
+X = (np.mean(images, axis=3)).reshape(n_samples, -1)
+
+# # or compute features using colors (3 times more features)
+# X = images.copy().reshape(n_samples, -1)
+
+# Scale features
+X -= np.mean(X, axis=0)
+X /= np.std(X, axis=0)
+
+#%%
+####################################################################
+# Split data into a half training and half test set
+# X_train, X_test, y_train, y_test, images_train, images_test = \
+#    train_test_split(X, y, images, test_size=0.5, random_state=0)
+# X_train, X_test, y_train, y_test = \
+#    train_test_split(X, y, test_size=0.5, random_state=0)
+
+indices = np.random.permutation(X.shape[0])
+train_idx, test_idx = indices[:X.shape[0] // 2], indices[X.shape[0] // 2:]
+X_train, X_test = X[train_idx, :], X[test_idx, :]
+y_train, y_test = y[train_idx], y[test_idx]
+images_train, images_test = images[
+    train_idx, :, :, :], images[test_idx, :, :, :]
+
+####################################################################
+# Quantitative evaluation of the model quality on the test set
+
+# %%
+# Q4
+print("--- Linear kernel ---")
+print("Fitting the classifier to the training set")
+t0 = time()
+
+# fit a classifier (linear) and test all the Cs
+Cs = 10. ** np.arange(-5, 6)
+scores = []
+for C in Cs:
+    clf_tmp = SVC(kernel='linear', C=C)
+    clf_tmp.fit(X_train, y_train)
+    # 这里用测试集分数来挑 C（课程里通常会再套一层验证集/交叉验证；本 TP 用固定半数当测试集）
+    scores.append(clf_tmp.score(X_test, y_test))
+
+ind = np.argmax(scores)
+print("Best C: {}".format(Cs[ind]))
+
+plt.figure()
+plt.plot(Cs, scores)
+plt.xlabel("Parametres de regularisation C")
+plt.ylabel("Scores d'apprentissage")
+plt.xscale("log")
+plt.tight_layout()
+plt.show()
+print("Best score: {}".format(np.max(scores)))
+
+print("Predicting the people names on the testing set")
+t0 = time()
+
+#%%
+# predict labels for the X_test images with the best classifier
+clf = SVC(kernel='linear', C=Cs[ind])
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+print("done in %0.3fs" % (time() - t0))
+# The chance level is the accuracy that will be reached when constantly predicting the majority class.
+print("Chance level : %s" % max(np.mean(y), 1. - np.mean(y)))
+print("Accuracy : %s" % clf.score(X_test, y_test))
+
+#%%
+####################################################################
+# Qualitative evaluation of the predictions using matplotlib
+
+prediction_titles = [title(y_pred[i], y_test[i], names)
+                     for i in range(y_pred.shape[0])]
+
+plot_gallery(images_test, prediction_titles)
+plt.show()
+
+####################################################################
+# Look at the coefficients
+plt.figure()
+plt.imshow(np.reshape(clf.coef_, (h, w)))
+plt.show()
+
+
+# %%
+# Q5
+
+def run_svm_cv(_X, _y):
+    _indices = np.random.permutation(_X.shape[0])
+    _train_idx, _test_idx = _indices[:_X.shape[0] // 2], _indices[_X.shape[0] // 2:]
+    _X_train, _X_test = _X[_train_idx, :], _X[_test_idx, :]
+    _y_train, _y_test = _y[_train_idx], _y[_test_idx]
+
+    _parameters = {'kernel': ['linear'], 'C': list(np.logspace(-3, 3, 5))}
+    _svr = svm.SVC()
+    _clf_linear = GridSearchCV(_svr, _parameters)
+    _clf_linear.fit(_X_train, _y_train)
+
+    print('Generalization score for linear kernel: %s, %s \n' %
+          (_clf_linear.score(_X_train, _y_train), _clf_linear.score(_X_test, _y_test)))
+
+print("Score sans variable de nuisance")
+# use run_svm_cv on original data
+run_svm_cv(X, y)
+
+print("Score avec variable de nuisance")
+n_features = X.shape[1]
+# On rajoute des variables de nuisances
+sigma = 1
+noise = sigma * np.random.randn(n_samples, 300, ) 
+#with gaussian coefficients of std sigma
+X_noisy = np.concatenate((X, noise), axis=1)
+X_noisy = X_noisy[np.random.permutation(X.shape[0])]
+# use run_svm_cv on noisy data
+run_svm_cv(X_noisy, y)
+
+
+#%%
+# Q6
+print("Score apres reduction de dimension")
+
+# 1) 依然从“带噪声”的高维特征出发
+#    注意：我们自己在这里重新划分一次 train/test，保证 PCA 只在训练集上拟合
+indices_pca = np.random.permutation(X_noisy.shape[0])
+train_idx_pca = indices_pca[:X_noisy.shape[0] // 2]
+test_idx_pca  = indices_pca[X_noisy.shape[0] // 2:]
+
+Xn_train = X_noisy[train_idx_pca, :]
+Xn_test  = X_noisy[test_idx_pca, :]
+yn_train = y[train_idx_pca]
+yn_test  = y[test_idx_pca]
+
+# 2) PCA：只用训练集“fit”，再 transform 训练/测试
+n_components = 12  # 可调整：20/50/100 试试看
+pca = PCA(n_components=n_components, svd_solver='randomized', whiten=True, random_state=0)
+pca.fit(Xn_train)                # 只在训练集上拟合
+Xtr = pca.transform(Xn_train)    # 训练集降维
+Xte = pca.transform(Xn_test)     # 测试集用同一个变换降维
+
+# （可选）看看累计解释方差，有助于选择 n_components
+plt.figure()
+plt.plot(np.cumsum(pca.explained_variance_ratio_))
+plt.xlabel("n_components")
+plt.ylabel("explained variance (cumulative)")
+plt.tight_layout()
+plt.show()
+
+# 3) 在降维后的数据上跑线性 SVM + 小网格的 C
+parameters = {'kernel': ['linear'], 'C': list(np.logspace(-3, 3, 5))}
+svr = svm.SVC()
+clf_pca = GridSearchCV(svr, parameters)
+clf_pca.fit(Xtr, yn_train)
+
+print('Generalization score with PCA (train, test): %s, %s \n' %
+      (clf_pca.score(Xtr, yn_train), clf_pca.score(Xte, yn_test)))
 # %%
